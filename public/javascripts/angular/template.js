cae2bc814161fa1d668651104cdd6d530e08657a
tinwy <tinweiyang@gmail.com>
2015-11-08 05:37:01 -0800

    Done with summary view
    Post summary screen to be implemented

--- public/javascripts/angular/template.js
+++ public/javascripts/angular/template.js
@@ -5,10 +5,13 @@ var app = angular.module("myApp", []);
 app.controller("cityCtrl", function ($scope) {
 		"use strict";
 	$scope.cities = [
-		{ name: "San Francisco"},
-		{ name: "New York"},
-		{ name: "Chicago"},
-		{ name: "Salt Lake City"},
-		{ name: "Miami"}
+/*		{ name: "San Francisco", num:"16 Events", pricing: "Flights from $47", img:"SF" }, */
+		{ name: "New York", num:"8 Events",  pricing: "Flights from $247", img:"NYC"},
+	/*	{ name: "Chicago", num:"9 Events",  pricing: "Flights from $156", img:""}, */
+		{ name: "Los Angeles", num:"6 Events",  pricing: "Flights from $84", img:"LA"}, 
+		{ name: "Miami", num:"12 Events",  pricing: "Flights from $322", img:"M"},
+		{ name: "Vegas", num:"10 Events",  pricing: "Flights from $154", img:"V"},
+		{ name: "Houston", num:"5 Events",  pricing: "Flights from $98", img:"H"},
+	/*	{ name: "Atlantic City", num:"7 Events",  pricing: "Flights from $376", img:""} */
 	];
 });
