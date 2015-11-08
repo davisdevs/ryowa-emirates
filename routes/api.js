0fd60e837870270e89079a20b7e207fd796452f1
davis12529 <davis12529@hotmail.com>
2015-11-08 01:43:28 -0800

    WIP API 2

--- routes/api.js
+++ routes/api.js
@@ -7,16 +7,32 @@ var categories = require("../modules/categories");
 router.get('/events/categories', function(req, res) {
   var namesArray = categories.getAllCategoryNames();
   res.json({
-      message: 'List of all categories',
-      data: namesArray
-      }
-  );
+    message: 'List of all categories',
+    data: namesArray
+  }
+);
 });
 
 /* Get list of events that fit */
-router.get('/events/categories/:id', function(req, res) {
-  var data = events.searchCategoriesXola();
-    res.json({ data: data});
+router.get('/events', function(req, res) {
+  var categoryArray = req.query.cat;
+  if (typeof categoryArray === "string") {
+    categoryArray = [categoryArray];
+  }
+  for (var i=0; i < categoryArray.length; i++) {
+    categoryArray[i] = decodeURIComponent(categoryArray[i]);
+  }
+  console.log("Searching for: " + categoryArray);
+  new Promise(function(resolve, reject) {
+    resolve(events.search(categoryArray));
+  }).then(function(data) {
+    // console.log(data);
+    res.json(JSON.stringify({
+      message: "Event Results",
+      data: data
+    }));
+
+  })
 });
 
 module.exports = router;
