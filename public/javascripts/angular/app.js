f5580c87cb9c3706ad69b22f2a6237c35d01e90f
Jonathan Lim <jonathan@1sb.com>
2015-11-08 04:34:49 -0800

    Completed functionality of Suggestions view

--- public/javascripts/angular/app.js
+++ public/javascripts/angular/app.js
@@ -1,4 +1,143 @@
 var app = angular.module('driftApp', ['ngRoute']);
+var data = [
+	{title: "Maroon 5 with Tove Lo & Phrases",
+	date: "16 Oct",
+	text: "DescriptionDescriptionDescription" +
+	"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+	"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+	"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+	"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+	"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+	"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+	"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+	"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+	"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+	"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+	"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
+	img: "https://rocktotick.com/wp-content/uploads/2015/02/Maroon-5-Bio.jpg"},
+	{title: "Avril Lavigne: What the F**K was I thinking?",
+		date: "22 Nov",
+		text: "DescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
+		img: "http://resources1.news.com.au/images/2013/05/02/1226632/958569-avril-lavigne.jpg"},
+	{title: "Kodaline: South East Asian Tour",
+		date: "03 Nov",
+		text: "DescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
+		img: "http://i.telegraph.co.uk/multimedia/archive/02473/kodaline_2473544b.jpg"},
+	{title: "Jay Chou",
+		date: "17 Nov",
+		text: "DescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
+		img: "http://i1213.photobucket.com/albums/cc478/Crazy4you93/jaychou.jpg"},
+	{title: "Sara Bareilles",
+		date: "24 Dec",
+		text: "DescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
+		img: "http://www.elliottbaybook.com/sites/elliottbaybook.com/files/sara-bareilles-2013-650-430.jpg"},
+	{title: "Linkin Park: Rage and War",
+		date: "28 Dec",
+		text: "DescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
+		img: "http://static1.gamespot.com/uploads/original/1539/15391776/2550913-4650940812-linki.jpg"},
+	{title: "Lakers Game @ McGraw Sports Stadium",
+		date: "31 Dec",
+		text: "DescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
+		img: "http://cdnstatic.visualizeus.com/thumbs/a0/44/basketball,dunk,lakers,nba-a044ccfd2cebddbafb96e6861c845fbd_h.jpg"},
+	{title: "Manchester United vs. West Brom",
+		date: "Old Trafford Stadium",
+		text: "DescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
+		img: "http://static01.nyt.com/images/2011/08/24/sports/soccer/24iht-soccer24/24iht-soccer24-articleLarge-v2.jpg"},
+	{title: "US Rugby Big League",
+		date: "1 Jan",
+		text: "DescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
+		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
+		img: "http://golivesportscast.com/wp-content/uploads/2015/08/USARugby.jpg"}
+];
+
+var savedSuggestions = [];
 
 app.config(['$routeProvider', function ($routeProvider, $locationProvider) {
 	$routeProvider
@@ -47,27 +186,44 @@ app.controller('CategoriesViewController', ['$scope', function($scope){
 
 app.controller('SuggestionsModalController', ['$scope', function($scope){
 	$scope.isModalVisible = false;
-	$scope.data = {title: "Maroon 5 with Tove Lo & Phrases",
-		date: "16 Oct",
-		text: "DescriptionDescriptionDescription" +
-		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
-		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
-		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription" +
-		"DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
-		img: "https://rocktotick.com/wp-content/uploads/2015/02/Maroon-5-Bio.jpg"};
+	$scope.data;
 	$scope.closeModal = function(){
 		$scope.isModalVisible = false;
-	}
-	$scope.showModal = function(){
+	};
+	$scope.showModal = function(suggestionObj){
+		$scope.data = suggestionObj;
 		$scope.isModalVisible = true;
-	}
+	};
 
+	//Toggle select/unselect drift button
+	var unselectedButtonUrl = "../assets/internal-modal-unclicked.png";
+	var selectedButtonUrl = "../assets/internal-modal-clicked.png";
+	$scope.driftSaveButtonUrl = unselectedButtonUrl;
+	$scope.saveButtonClicked = function(){
+		if($scope.driftSaveButtonUrl !== selectedButtonUrl) {
+			//Save event
+			$scope.driftSaveButtonUrl = selectedButtonUrl;
+			$scope.data['isSaved'] = true;
+			//Add to saved array
+			savedSuggestions.push($scope.data['isSaved']);
+
+
+		} else {
+			//Unsave event
+			$scope.driftSaveButtonUrl = unselectedButtonUrl;
+			$scope.data['isSaved'] = false;
+			//Removed from saved array
+			savedSuggestions.filter(function(obj){
+				return obj.title !== $scope.data.title;
+			});
+		}
+	};
 }]);
 
 app.controller('SuggestionsViewController', ['$scope', function($scope){
 	$scope.header = "Suggestions View";
 	$scope.cities = ['NY', 'SF', 'LA', 'LV'];
-
+	$scope.suggestions = data;
 	//Tabs functionality
 	$scope.currentTab = 1;
 	$scope.tabPressed = function(tabNum){
@@ -86,5 +242,6 @@ app.controller('SuggestionsViewController', ['$scope', function($scope){
 				break;
 		}
 	};
+
 }]);
 
