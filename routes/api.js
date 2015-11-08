c84c304fe29f624d45ef925a306f3bebc6888dab
davis12529 <davis12529@hotmail.com>
2015-11-07 16:41:34 -0800

    [Update] GET /api/events/categories returns an array of category names

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var events = require("../modules/events");
+var categories = require("../modules/categories");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET list of all categories with name and id*/
+router.get('/events/categories', function(req, res) {
+  var namesArray = categories.getAllCategoryNames();
+  res.json({
+      message: 'List of all categories',
+      data: namesArray
+      }
+  );
+});
+
+/* Get list of events that fit */
+router.get('/events/categories/:id', function(req, res) {
+  var data = events.searchCategoriesXola();
+    res.json({ data: data});
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET list of all categories with name and id*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
 });
+
 
 module.exports = router;
