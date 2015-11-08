0406b56e60dd4695ba6598935423e1e48c90bfe6
Jonathan Lim <jonathan@1sb.com>
2015-11-08 06:31:36 -0800

    Merging

--- public/javascripts/angular/app.js
+++ public/javascripts/angular/app.js
@@ -331,3 +331,16 @@ app.controller("ItemCtrl", function ($scope, $http) {
 	};
 
 });
+
+app.controller('cityController', ['$scope', function ($scope) {
+	$scope.cities = [
+		/*		{ name: "San Francisco", num:"16 Events", pricing: "Flights from $47", img:"SF" }, */
+		{ name: "New York", num:"8 Events",  pricing: "Flights from $247", img:"NYC"},
+		/*	{ name: "Chicago", num:"9 Events",  pricing: "Flights from $156", img:""}, */
+		{ name: "Los Angeles", num:"6 Events",  pricing: "Flights from $84", img:"LA"},
+		{ name: "Miami", num:"12 Events",  pricing: "Flights from $322", img:"M"},
+		{ name: "Vegas", num:"10 Events",  pricing: "Flights from $154", img:"V"},
+		{ name: "Houston", num:"5 Events",  pricing: "Flights from $98", img:"H"}
+		/*	{ name: "Atlantic City", num:"7 Events",  pricing: "Flights from $376", img:""} */
+	];
+}]);
